cc4e1962c077643fb69faa5df6d12d3dbd66bac2
Eldwin Fikhar Ananda <eldwin.fikhar231104@gmail.com>
2023-07-11 22:03:12 +0700

    create meet4.go
    added the func main of meet4.go

---
+++ meet4.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Kel. 4 Pra Intern ASE Lab 2023")
+	fmt.Println("1. Eldwin Fikhar Ananda [1303220081]")
+	fmt.Println("2. Fauzan Almas Saepullah [1301210558]")
+}
